74fee7f42ab3cd7c93c576687ca5c7d9dabf442d
Aparna <aparnajay.jay@gmail.com>
2017-07-01 21:40:52 -0700

    Design Artist view and add link to Album page to Artist page

--- app/src/main/java/com/example/android/musicplayer/ArtistView.java
+++ app/src/main/java/com/example/android/musicplayer/ArtistView.java
@@ -1,7 +1,10 @@
 package com.example.android.musicplayer;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class ArtistView extends AppCompatActivity {
 
@@ -9,5 +12,21 @@ public class ArtistView extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_artist_view);
+
+        // Find the View that store the album
+        ImageView theBlessedUnrest = (ImageView) findViewById(R.id.blessed_unrest_album);
+
+        // Set a click listener on that View
+        theBlessedUnrest.setOnClickListener( new View.OnClickListener() {
+            // The code in this method will be executed when the numbers category is clicked on.
+            @Override
+            public void onClick(View view) {
+                // Create a new intent to open the {@link NumbersActivity}
+                Intent AlbumIntent = new Intent(ArtistView.this, AlbumView.class);
+
+                // Start the new activity
+                startActivity(AlbumIntent);
+            }
+        });
     }
 }
